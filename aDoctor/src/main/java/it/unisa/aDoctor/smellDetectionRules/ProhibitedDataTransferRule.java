735d0f144927c65e60a34dc2ef4130118b67a196
aKsHay <tcs15b005@iittp.ac.in>
2018-02-06 13:55:29 +0530

    Update ProhibitedDataTransferRule.java

--- aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/ProhibitedDataTransferRule.java
+++ aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/ProhibitedDataTransferRule.java
@@ -1,1 +1,20 @@
+package it.unisa.aDoctor.smellDetectionRules;
 
+import java.io.IOException;
+
+import it.unisa.aDoctor.beans.ClassBean;
+
+public class ProhibitedDataTransferRule {
+
+    public boolean isProhibitedDataTransfer(ClassBean pClassBean) throws IOException {
+
+        if (pClassBean.getTextContent().contains("HttpPost ")||pClassBean.getTextContent().contains("NfcAdapter ")||
+            pClassBean.getTextContent().contains("NetworkFragment ")||pClassBean.getTextContent().contains("HttpURLConnection")) {
+            if (!(pClassBean.getTextContent().contains("NetworkInfo")&&pClassBean.getTextContent().contains("ConnectivityManager"))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
