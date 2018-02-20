304ff9855e1d543551a74261101cf1bb8e34345f
Tejas-11 <tcs15b028@iittp.ac.in>
2018-02-20 07:37:22 +0530

    Update DebuggableReleaseRule.java
    Repaired the class

--- aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/DebuggableReleaseRule.java
+++ aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/DebuggableReleaseRule.java
@@ -11,12 +11,12 @@ import org.apache.commons.io.LineIterator;
 public class DebuggableReleaseRule {
 
     public boolean isDebuggableRelease(File androidManifest) throws IOException {
-        Pattern regex = Pattern.compile("(.*)android:debuggable(\\s*)=(\\s*)\"true\"", Pattern.MULTILINE);
+        Pattern regex = Pattern.compile(".*android:debuggable\\s*=\\s*\"true\".*", Pattern.MULTILINE);
         LineIterator iter = FileUtils.lineIterator(androidManifest);
         while (iter.hasNext()) {
             String row = iter.next();
             Matcher regexMatcher = regex.matcher(row);
-            if (regexMatcher.find()) {
+            if (regexMatcher.matches()) {
                 return true;
             }
         }
