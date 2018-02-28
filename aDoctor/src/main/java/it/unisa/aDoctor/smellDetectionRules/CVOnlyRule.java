c2bc2dcfdfd4516313a176556d5e3b7482863567
vikassubramaniam <vikas.subramaniam@gmail.com>
2018-02-28 13:32:54 +0530

    CVOnly added

---
+++ aDoctor/src/main/java/it/unisa/aDoctor/smellDetectionRules/CVOnlyRule.java
@@ -0,0 +1,26 @@
+package it.unisa.aDoctor.smellDetectionRules;
+
+import java.io.IOException;
+import it.unisa.aDoctor.beans.ClassBean;
+
+
+public class CVOnlyRule {
+	public boolean isCVOnly(ClassBean pClassBean) throws IOException {
+
+		if (pClassBean.getTextContent().contains("implements X509TrustManager")) {
+            
+           if((pClassBean.getTextContent().contains("checkValidity()")
+        		 &&!(pClassBean.getTextContent().contains("getNotAfter()")
+        		 ||pClassBean.getTextContent().contains("getNotBefore()")
+        		 ||pClassBean.getTextContent().contains("getSigAlgName()")
+        		 ||pClassBean.getTextContent().contains("getSigAlgoID()")
+        		 ||pClassBean.getTextContent().contains("getTBSCertificate()")))) {
+			
+			return true;
+           }
+    }
+
+        return false;
+    }
+	
+}
